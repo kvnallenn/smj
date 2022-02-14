f4cdf6d8452610192fdb083d0cb19509aac1977c
kvnallenn <kvnallenn@gmail.com>
2022-02-14 16:32:38 +0700

    Perbaikan Content Login Form.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,9 +10,9 @@ export const Login = () => {
         <NavbarTop />
         <NavbarBot />
         <section className="loginpage">
-            <div className='main'>
+            <div className='main-login'>
             <div className="container col-xl-9 col-xxl-8 px-5 py-5">
-                <div className="row align-items-center g-lg-5 py-5">
+                <div className="align-items-center g-lg-5 py-5">
                     <LoginForm/>
                 </div>
             </div>
